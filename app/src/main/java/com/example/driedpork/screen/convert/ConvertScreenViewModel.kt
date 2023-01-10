16d86898678dddd148b674d13856c0e72d0157ba
ChuangWei Ma <chungwwei223@gmail.com>
2023-01-10 13:19:24 -0500

    perf: use suspend fn to honor the latest convert request

--- app/src/main/java/com/example/driedpork/screen/convert/ConvertScreenViewModel.kt
+++ app/src/main/java/com/example/driedpork/screen/convert/ConvertScreenViewModel.kt
@@ -1,18 +1,18 @@
 package com.example.driedpork.screen.convert
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.driedpork.model.coingecko.Market
 import com.example.driedpork.screen.home.HomeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-data class ConvertedCoinItem (
+data class ConvertedCoinItem(
     val id: String,
     val symbol: String,
     val name: String,
@@ -33,36 +33,39 @@ class ConvertScreenViewModel @Inject constructor(
     private val _uiState = MutableStateFlow(ConvertScreenUiState())
     val uiState: StateFlow<ConvertScreenUiState> = _uiState.asStateFlow()
 
+    private var convertPriceJob: Job? = null
+
+
     init {
         getCoins()
     }
 
+
     fun submitValueChange(amount: String) {
-        // convert the amount to double
-        Log.d("ConvertScreenViewModel", "submitValueChange: $amount")
-        val amountDouble = amount.toDoubleOrNull() ?: 0.0
-        val convertedCoinsList = _uiState.value.coinsList.map {
-            ConvertedCoinItem(
-                id = it.id,
-                symbol = it.symbol,
-                name = it.name,
-                image = it.image,
-                convertedPrice = String.format("%.10f", amountDouble / it.currentPrice).toDouble()
-            )
+        convertPriceJob?.cancel()
+        convertPriceJob = viewModelScope.launch {
+            // convert the amount to double
+            val amountDouble = amount.toDoubleOrNull() ?: 0.0
+            val convertedCoinsList = _uiState.value.coinsList.map {
+                ConvertedCoinItem(
+                    id = it.id,
+                    symbol = it.symbol,
+                    name = it.name,
+                    image = it.image,
+                    convertedPrice = String.format("%.10f", amountDouble / it.currentPrice)
+                        .toDouble()
+                )
+            }
+            // recompose
+            _uiState.value = _uiState.value.copy(convertedCoinsList = convertedCoinsList)
         }
-        // recompose
-        _uiState.value = _uiState.value.copy(convertedCoinsList = convertedCoinsList)
     }
 
-//    fun submitUnitChange(unit: String) {
-//
-//    }
 
     private fun getCoins() {
         viewModelScope.launch {
             homeRepository.coinsList.collect { coinsList ->
-                _uiState.value = _uiState.value.copy(
-                    coinsList = coinsList,
+                _uiState.value = _uiState.value.copy(coinsList = coinsList,
                     convertedCoinsList = coinsList.take(20).map {
                         ConvertedCoinItem(
                             id = it.id,
@@ -71,8 +74,7 @@ class ConvertScreenViewModel @Inject constructor(
                             image = it.image,
                             convertedPrice = 1.0 / it.currentPrice
                         )
-                    }
-                )
+                    })
             }
         }
     }
